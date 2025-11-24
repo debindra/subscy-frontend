c6a0650734d216e6049bc6ffda7731653bdf33fc
Deb <deb.katwal@gmail.com>
2025-11-24 11:38:43 +0545

    fix ui

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -126,7 +126,7 @@ export const Navbar: React.FC = () => {
                   )}
                 </Link>
 
-                {hasBusinessContext && (
+                {/* {hasBusinessContext && (
                   <Link href="/dashboard/business" className={navLinkClass('/dashboard/business')}>
                     <div className="flex items-center space-x-2">
                       <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -138,10 +138,10 @@ export const Navbar: React.FC = () => {
                       <div className="absolute bottom-0 left-0 right-0 h-0.5 bg-gradient-to-r from-primary-600 to-primary-800 dark:from-primary-400 dark:to-primary-600"></div>
                     )}
                   </Link>
-                )}
+                )} */}
 
                 {/* Account Switcher */}
-                <AccountSwitcher />
+                {/* <AccountSwitcher /> */}
 
                 {/* Dark Mode Toggle */}
                 <button
